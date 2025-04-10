ea4e956de5cfa0d2c78772b929dc4276cbda65da
Robert H Stolz <7143905+garden-of-delete@users.noreply.github.com>
2025-04-09 22:40:38 -0700

    fix main

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ func main() {
 
 	//gene := src.NewGene("res/pfc53_full.fa")
 	gene := src.NewGene("res/gattaca.fa")
-	rloopModel := src.NewParamsReasonableDefaults() // best set of experimentally validated params
-	structures := gene.ComputeStructures(&rloopModel)
+	// rloopModel := src.NewParamsReasonableDefaults() // best set of experimentally validated params
+	// structures := gene.ComputeStructures(&rloopModel)
 
 	windows := src.FromLinearWindows(gene.Sequence, 2)
 	fmt.Println(windows)
 
-	for _, w := range windows {
-		w.Start
-	}
+	// for _, w := range windows {
+	// 	w.Start
+	// }
 
 	fmt.Println(gene.GeneName)
 }
